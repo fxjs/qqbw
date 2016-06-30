df9afcbe8cbe3f62eb4381f7348f69841df62b4a
fxjs <1026344755@qq.com>
2016-06-30 18:00:58 +0800

    加入material前端库，修复导航栏遍历bug，数据表xxxx_item 新增itemurl字段

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -4,21 +4,21 @@
  * @type {Object}
  */
 export default {
-  type: 'mysql',
-  log_sql: true,
-  log_connect: true,
-  adapter: {
-    mysql: {
-      host: '127.0.0.1',
-      port: '3306',
-      database: 'liblog',
-      user: 'root',
-      password: 'root',
-      prefix: 'li_',
-      encoding: 'utf8'
-    },
-    mongo: {
+    type: 'mysql',
+    log_sql: true,
+    log_connect: true,
+    adapter: {
+        mysql: {
+            host: '127.0.0.1',
+            port: '3307',
+            database: 'liblog',
+            user: 'root',
+            password: 'usbw',
+            prefix: 'li_',
+            encoding: 'utf8'
+        },
+        mongo: {
 
+        }
     }
-  }
 };
--- src/home/controller/base.js
+++ src/home/controller/base.js
@@ -1,38 +1,55 @@
 'use strict';
 
 export default class extends think.controller.base {
-  /**
-   * some base method in here
-   */
+    /**
+     * some base method in here
+     */
     async __before() {
         await this.getConfig();
         //设置CSRF值
-        let csrf=await this.session("__CSRF__");
-        this.assign("csrf",csrf);
-        
+        let csrf = await this.session("__CSRF__");
+        this.assign("csrf", csrf);
+
+        // 获取导航栏
+        let webNavs = await this.model("item").order("id").select();
+        this.assign("webNavs", webNavs);
+        console.log("FX->", webNavs);
+
         //获取tags
-        let tagList=await this.model("tags").select(); 
-        this.assign('tagList',tagList); 
-        
+        let tagList = await this.model("tags").select();
+        this.assign('tagList', tagList);
+
         //获取图文推荐列表
-        let picrecomList=await this.model("article").where({topicrecom:1,ispublished:1}).limit(5).select();
-        this.assign("picrecomList",picrecomList);
-        
+        let picrecomList = await this.model("article").where({
+            topicrecom: 1,
+            ispublished: 1
+        }).limit(5).select();
+        this.assign("picrecomList", picrecomList);
+
         //获取站长推荐列表
-        let torecomList=await this.model("article").where({torecom:1,ispublished:1}).limit(5).select();
-        this.assign("torecomList",torecomList);
-        
+        let torecomList = await this.model("article").where({
+            torecom: 1,
+            ispublished: 1
+        }).limit(5).select();
+        this.assign("torecomList", torecomList);
+
         //获取点击排行列表
-        let popularList=await this.model("article").where({ispublished:1}).order("view DESC").limit(5).select();
-        this.assign("popularList",popularList);  
-              
-         //获取最新文章列表
-        let newestList=await this.model("article").where({ispublished:1}).order("createtime DESC").limit(5).select();
-        this.assign("newestList",newestList);                 
-                
+        let popularList = await this.model("article").where({
+            ispublished: 1
+        }).order("view DESC").limit(5).select();
+        this.assign("popularList", popularList);
+
+        //获取最新文章列表
+        let newestList = await this.model("article").where({
+            ispublished: 1
+        }).order("createtime DESC").limit(5).select();
+        this.assign("newestList", newestList);
+
     }
     async getConfig() {
-        let sysdata=await this.model('system').where({id:1}).find();
-        this.assign('_web',sysdata);
+        let sysdata = await this.model('system').where({
+            id: 1
+        }).find();
+        this.assign('_web', sysdata);
     }
 }
